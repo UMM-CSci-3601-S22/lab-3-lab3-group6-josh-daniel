26129d136cdc3871f938668dabcbef0dabaf318f
Joshua Engelkes <enge0479@morris.umn.edu>
2022-02-17 16:28:15 -0600

    ng generate todo components for service, card, and list
    Coauthored by: Daniel Albright

---
+++ client/src/app/todos/todos-card/todos-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodosCardComponent } from './todos-card.component';
+
+describe('TodosCardComponent', () => {
+  let component: TodosCardComponent;
+  let fixture: ComponentFixture<TodosCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TodosCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
