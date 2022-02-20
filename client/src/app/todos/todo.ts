c32f2e54d78bcd4751105b56dd41d35a4f1e56a3
Daniel Albright <albri118@morris.umn.edu>
2022-02-19 23:45:05 -0600

    Order interface to match server-side
    This is unnecessary, but it was bothering me. Totally worthy of a separate commit.

--- client/src/app/todos/todo.ts
+++ client/src/app/todos/todo.ts
@@ -1,7 +1,7 @@
 export interface Todo {
-  _id: string;
-  owner: string;
-  category: string;
-  status: boolean;
-  body: string;
+  _id: string; // Server side filter
+  owner: string; // local filter
+  status: boolean; // Server side filter
+  body: string; // Server side filter
+  category: string; // Server side filter
 }
