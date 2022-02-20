e6ff78c20bbd558d9e2d46df60b46fe053e96bc9
Daniel Albright <albri118@morris.umn.edu>
2022-02-19 23:46:50 -0600

    Start Karma tests for TodoService

--- client/src/app/todos/todo.service.spec.ts
+++ client/src/app/todos/todo.service.spec.ts
@@ -5,15 +5,93 @@ import { Todo } from './todo';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  let service: TodoService;
+  // Todos for testing
+  const testTodos: Todo[] = [
+    {
+      _id: 'arthur_id',
+      owner: 'Arthur',
+      category: 'Camp Task',
+      status: false,
+      body: 'Dutch needs more money'
+    },
+    {
+      _id: 'daniel_id',
+      owner: 'Daniel',
+      category: 'Homework',
+      status: false,
+      body: 'Need to work on lab 3'
+    },
+    {
+      _id: 'lucy_id',
+      owner: 'Lucy',
+      category: 'Dog Stuff',
+      status: true,
+      body: 'Think dog thoughts'
+    }
+  ];
+
+  let todoService: TodoService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    todoService = new TodoService(httpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  describe('getTodos()', () => {
+
+    it('calls `api/todos` when `getTodos()` is called with no parameters', () => {
+      // Make sure we're getting our (unfiltered) testUsers when
+      // getTodos() is called sin params.
+      todoService.getTodos().subscribe(
+        todos => expect(todos).toBe(testTodos)
+      );
+
+      // Specify that (exactly) one request will be made to the specified URL.
+      const req = httpTestingController.expectOne(todoService.todoUrl);
+      // Check that the request made to that URL was a GET request.
+      expect(req.request.method).toEqual('GET');
+      // Check that the request had no query parameters.
+      expect(req.request.params.keys().length).toBe(0);
+      // Specify the content of the response to that request. This
+      // triggers the subscribe above, which leads to that check
+      // actually being performed.
+      req.flush(testTodos);
+    });
+  });
+
+  describe('Calling getTodos() with parameters correctly forms the HTTP request', () => {
+
+    it('correctly calls api/todos with filter parameter \'status\'', () => {
+      todoService.getTodos({ status: 'complete' }).subscribe(
+        todos => expect(todos).toBe(testTodos)
+      );
+
+      // Specify that (exactly) one request will be made to the specified URL with the status parameter.
+      const req = httpTestingController.expectOne(
+        (request) => request.url.startsWith(todoService.todoUrl) && request.params.has('status')
+      );
+
+      // Check that the request made to that URL was a GET request.
+      expect(req.request.method).toEqual('GET');
+
+      // Check that the role parameter was 'admin'
+      expect(req.request.params.get('status')).toEqual('complete');
+
+      req.flush(testTodos);
+    });
   });
 
 });
