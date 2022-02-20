853f99005dc239ec9b68d3a0cbe009d87619c194
Daniel Albright <albri118@morris.umn.edu>
2022-02-19 23:45:58 -0600

    Setup header of todo.service

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Todo } from './todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  // todos server api url:
+  readonly todoURL: string = environment.apiUrl + 'todos';
 
-  constructor() { }
+  // Inject an HttpClient
+  constructor(private httpClient: HttpClient) { }
 }
